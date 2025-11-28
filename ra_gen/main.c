cc7a140f461ce4c734e6c0a37490ccdab7be3c10
Keerthi <kmallesh@technosofteng.com>
2025-11-28 12:44:17 +0530

    1. Added the UART thread and uart initialization.

--- ra_gen/main.c
+++ ra_gen/main.c
@@ -5,6 +5,8 @@
 #include "semphr.h"
 extern void CAN_thread_create(void);
 extern TaskHandle_t CAN_thread;
+extern void uart_thread_create(void);
+extern TaskHandle_t uart_thread;
 uint32_t g_fsp_common_thread_count;
 bool g_fsp_common_initialized;
 SemaphoreHandle_t g_fsp_common_initialized_semaphore;
@@ -103,6 +105,7 @@ int main(void)
 
     /* Init RTOS tasks. */
     CAN_thread_create ();
+    uart_thread_create ();
 
     /* Start the scheduler. */
     vTaskStartScheduler ();
---
+++ ra_gen/uart_thread.h
@@ -0,0 +1,29 @@
+/* generated thread header file - do not edit */
+#ifndef UART_THREAD_H_
+#define UART_THREAD_H_
+#include "bsp_api.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "hal_data.h"
+#ifdef __cplusplus
+                extern "C" void uart_thread_entry(void * pvParameters);
+                #else
+extern void uart_thread_entry(void *pvParameters);
+#endif
+#include "r_sci_uart.h"
+#include "r_uart_api.h"
+FSP_HEADER
+/** UART on SCI Instance. */
+extern const uart_instance_t g_uart0;
+
+/** Access the UART instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_uart_instance_ctrl_t g_uart0_ctrl;
+extern const uart_cfg_t g_uart0_cfg;
+extern const sci_uart_extended_cfg_t g_uart0_cfg_extend;
+
+#ifndef NULL
+void NULL(uart_callback_args_t *p_args);
+#endif
+FSP_FOOTER
+#endif /* UART_THREAD_H_ */
